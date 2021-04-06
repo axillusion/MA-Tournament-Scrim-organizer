00ed4e29399c233aad8ff92151b415127d2b362a
IlluSion <georgescu.calin.stefan@gmail.com>
2021-04-06 12:56:06 +0300

    Added setMMR command

--- Commands.py
+++ Commands.py
@@ -6,8 +6,24 @@ def __init ( command ):
 	for member in memberList:
 		memberMMR[member] = defaultMMR
 
-def executeCommand( command ):
-	if command.content.startswith( commandSyntax + "init" ):
+def __setMMR ( command ):
+	msg = command.content.split( " " )
+	name = msg[1]
+	mmr = msg[2]
+	for member in memberList:
+		if memberList[member].name == name:
+			memberMMR[member] = mmr
+
+async def executeCommand( ctx, command ):
+	msg = command.content
+	if msg.startswith( commandSyntax + "init" ):
 		__init ( command )
+	elif msg.startswith( commandSyntax + "setMMR" ):
+		if "admin" in [x.name.lower() for x in command.author.roles] or command.author.name == "IlluSion":
+			__setMMR ( command )
+		else:
+			client.send_message ( ctx.message.channel, "You need to be an admin to use that command" )
+		
+
 
 
--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import discord
 import private
 from Commands import executeCommand
 
-client = discord.Client()
-
 class MyClient(discord.Client):
 	async def on_ready(self):
 		print('Logged on as', self.user)
 
 	async def on_message( ctx, message ):
-		if message.content.startswith ( commandSyntax ):
+		if message.content.startswith ( ctx, commandSyntax ):
 			executeCommand ( message )
 
 client = MyClient()

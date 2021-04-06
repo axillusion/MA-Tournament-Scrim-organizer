4fde63b43df1588f9756221ceaea2fb6085517b8
IlluSion <georgescu.calin.stefan@gmail.com>
2021-04-06 22:38:35 +0300

    Bug fixes

--- Commands.py
+++ Commands.py
@@ -7,17 +7,17 @@ playersCount = []
 lastscrim = -1;
 
 def __init ( command ):
-	memberList = command.guild.members
-	for member in memberList:
-		memberMMR[member] = defaultMMR
+	private.memberList = command.guild.members
+	for i in range ( 0, len( private.memberList ) - 1):
+		private.memberMMR[i] = private.defaultMMR
 
-def __setMMR ( command ):
+def __setMmr ( command ):
 	msg = command.content.split( " " )
 	name = msg[1]
 	mmr = msg[2]
-	for member in memberList:
-		if memberList[member].name == name:
-			memberMMR[member] = mmr
+	for member in private.memberList:
+		if private.memberList[member].name == name:
+			private.memberMMR[member] = mmr
 
 def __startScrim ( command ):
 	players.clear()
@@ -29,21 +29,21 @@ def __endScrim ( command ):
 
 async def executeCommand( ctx, command ):
 	msg = command.content
-	if msg.startswith( commandSyntax + "init" ):
+	if msg.startswith( private.COMMANDSYNTAX + "init" ):
 		__init ( command )
-	elif msg.startswith( commandSyntax + "setMMR" ):
+	elif msg.startswith( private.COMMANDSYNTAX + "setMMR" ):
 		if "admin" in [x.name.lower() for x in command.author.roles] or command.author.name == "IlluSion":
-			__setMMR ( command )
+			__setMmr ( command )
 		else:
 			await client.send_message ( ctx.message.channel, "You need to be an admin to use that command" )
-	elif msg.startswith( commandSyntax + "start scrim" ):
+	elif msg.startswith( private.COMMANDSYNTAX + "start scrim" ):
 		__startScrim ( command )
-	elif msg.startswith ( commandSyntax + "join" ):
+	elif msg.startswith ( private.COMMANDSYNTAX + "join" ):
 		players.append ( command.author.name )
 		playerCount += 1
 		if playersCount == 8:
 			await client.send_message ( ctx.message.channel, "Scrim starting!\nTeam 1:" + players[0] + " & " + players[1] + "\n" + "Team 2:" + players[2] + " & " + players[3] + "\n" + "Team 3:" + players[4] + " & " + players[5] + "\n" + "Team 4:" + players[6] + " & " + players[7] + "\n")
-	elif msg.startswith ( commandSyntax + "end scrim"):
+	elif msg.startswith ( private.COMMANDSYNTAX + "end scrim"):
 		__endScrim ( command )
 
 
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import discord
 import private
-from Commands import executeCommand
+import Commands
 
 class MyClient(discord.Client):
 	async def on_ready(self):
 		print('Logged on as', self.user)
 
 	async def on_message( ctx, message ):
-		if message.content.startswith ( ctx, commandSyntax ):
-			executeCommand ( message )
+		if message.content.startswith ( private.COMMANDSYNTAX ):
+			await Commands.executeCommand ( ctx, message )
 
 client = MyClient()
 client.run( private.TOKEN )

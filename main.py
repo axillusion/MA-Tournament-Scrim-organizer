03358df6bcc18b87e7575f3f3a1a7f8ebb5513fe
IlluSion <georgescu.calin.stefan@gmail.com>
2021-04-12 14:18:30 +0300

    Added database support

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import Database
 import os
 import discord
 import private
-import Commands
+from Commands import executeCommand
+
+db_connection = None
 
 class MyClient(discord.Client):
 	async def on_ready(self):
 		print('Logged on as', self.user)
+		db_connection = Database.create_db_connection ( private.DatabaseHost, private.DatabaseUser, private.DatabasePassword, private.DatabaseName )
 
 	async def on_message( ctx, message ):
 		if message.content.startswith ( private.COMMANDSYNTAX ):
-			await Commands.executeCommand ( ctx, message )
+			await executeCommand ( ctx, message )
 
 client = MyClient()
 client.run( private.TOKEN )
